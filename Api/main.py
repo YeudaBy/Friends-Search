b4467d64e40bfa53429280d36c9749996beb5899
yeuda <yeudaborodyanski@gmail.com>
2021-12-01 19:32:01 +0200

    finish api

--- Api/main.py
+++ Api/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, render_template, Response
+from flask import Flask, request, jsonify, render_template
 from pony.orm import db_session
 from queries import search as find, Parse
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route("/")
@@ -17,9 +19,9 @@ def search():
     response = {}
 
     query = request.args.get("query") or request.args.get("q")
-    episode = request.args.get("episode") or request.args.get("e")
-    season = request.args.get("season") or request.args.get("s")
-    limit = request.args.get("limit") or request.args.get("l")
+    # episode = request.args.get("episode") or request.args.get("e")
+    # season = request.args.get("season") or request.args.get("s")
+    limit = request.args.get("limit") or request.args.get("l") or 50
     lang = request.args.get("lang") or request.args.get("language")
 
     if not query:
@@ -28,13 +30,13 @@ def search():
     if lang and len(lang) != 2:
         error = {"error": "`language` must be two characters, e.g. `EN`."}
 
-    if episode and not episode.isdigit():
-        error = {"error": "`episode` index must be a digit."}
+    # if episode and not episode.isdigit():
+    #     error = {"error": "`episode` index must be a digit."}
+    #
+    # if season and not season.isdigit():
+    #     error = {"error": "`season` index must be a digit."}
 
-    if season and not season.isdigit():
-        error = {"error": "`season` index must be a digit."}
-
-    if limit and not limit.isdigit():
+    if limit and isinstance(limit, str) and not limit.isdigit():
         error = {"error": "`limit` must be a digit."}
 
     if error:
@@ -43,16 +45,11 @@ def search():
     with db_session:
         results = [Parse(i).__dict__ for i in find(
             query=query,
-            season=season,
-            episode=episode,
-            limit=limit,
+            limit=int(limit),
             lang=lang
         )]
         response["count"] = len(results)
         response["results"] = results
-
-        # if response["count"] == 0:
-        #     return Response(status=404)
 
         return jsonify(response), 200
 
--- Api/queries.py
+++ Api/queries.py
@@ -12,41 +12,29 @@ class Parse:
         self.season = result.season.__str__()
 
 
-def make_episode(episode: int, season: int) -> str:
-    """ convert episode and season ints to one string """
-    return None if not episode and not season else f"{str(season).zfill(2)}-{str(episode).zfill(2)}"
+# def make_episode(episode: int, season: int) -> str:
+#     """ convert episode and season ints to one string """
+#     return None if not episode and not season else f"{str(season).zfill(2)}-{str(episode).zfill(2)}"
 
 
 @db_session
 def search(
         query: str,
-        season: int = None,
-        episode: int = None,
+        # season: int = None,
+        # episode: int = None,
         limit: int = None,
         lang: str = None
             ) -> List[Subtitle]:
     """
     search sentences on DataBase.
     :param query: sentence to search
-    :param season: in specific season, default None.
-    :param episode: in specific episode, default None.
     :return: list of results, contain: content, start and end time, and episode
     :param lang: language to search, default None
-    :param limit: limit of results count, default None
+    :param limit: limit of results count, default 50
     """
-
-    print(query, season, episode, lang, limit)
-
     query = Subtitle.clean_text(query)  # make clear query
 
     result = Subtitle.select(lambda i: query in i.raw_content)[:limit]
-
-    if episode:
-        result = list(filter(lambda i: i.episode == episode, result))
-
-    if season:
-        result = list(filter(lambda i: i.season == season, result))
-        print(result)
 
     if lang:
         result = list(filter(lambda i: i.lang == lang.lower(), result))
