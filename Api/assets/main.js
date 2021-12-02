7334b1d03b9c0b19a8e3fd94e9eedc3935e04557
yeuda <yeudaborodyanski@gmail.com>
2021-12-02 19:30:54 +0200

    css and finish web

--- Api/assets/main.js
+++
@@ -1,9 +0,0 @@
-function request(query) {
-    fetch(`/api/search?query=${query}`).
-    then(res => console.log(res))
-}
-
-function getResults() {
-    const query = document.getElementById("search-field")
-    request(query)
-}
