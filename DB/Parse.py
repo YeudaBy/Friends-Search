7fed839053420363872f25f9128e70ef4995c4a4
yeuda <yeudaborodyanski@gmail.com>
2021-12-06 05:53:13 +0200

    remove_tags from parser

--- DB/Parse.py
+++ DB/Parse.py
@@ -1,10 +1,12 @@
+import re
+
 
 class Parse:
     """ parse sql result to a class """
 
     def __init__(self, result):
         self.id = result.id
-        self.content = result.content.replace("\n", " ")
+        self.content = self.remove_tags(result.content)
         self.start = result.start
         self.end = result.end
         self.episode = result.episode.__str__()
@@ -24,3 +26,7 @@ class Parse:
 
     def __str__(self):
         return self.__dict__().__str__()
+
+    @staticmethod
+    def remove_tags(text: str) -> str:
+        return re.sub(r"<.*?>", "", text).replace("\n", " ")
