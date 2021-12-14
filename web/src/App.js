fe96823e11cac179081c5f3faffcfcc2ae1ddc7e
yeuda <yeudaborodyanski@gmail.com>
2021-12-14 17:59:57 +0200

    WEB: add lang and head component

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,18 @@
 import { Search } from "./components/Search";
+import Head from "./components/Head";
+import "./App.css"
+import { useState } from "react";
 
 function App() {
+  const [lang, setLangs] = useState(0);
+
+  function updateLang(newLang) { setLangs(newLang) }
+
   return (
-    <Search />
+    <>
+      <Head lang={lang}/>
+      <Search lang={lang}/>
+    </>
   );
 }
 
