86f028024b2107e0ffa8490f9853fcb0000ea99e
yeuda <yeudaborodyanski@gmail.com>
2021-12-14 17:39:26 +0200

    WEB: set langs for search

--- web/src/components/SelectLang.js
+++ web/src/components/SelectLang.js
@@ -5,7 +5,7 @@ const baseUrl = "http://127.0.0.1:8080/"
 export default class SelectLang extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {langs: []}
+        this.state = {langs: {}}
         this.loadLangs = this.loadLangs.bind(this);
     }
 
@@ -13,18 +13,23 @@ export default class SelectLang extends React.Component {
         fetch(baseUrl + "language")
             .then((res) => res.json())
             .then((res) => this.setState({langs: res}));
+            console.log(Object.keys(this.state.langs).length)
     }
     
     render() {
+        const langs = this.state.langs;
         return (
             <details>
-                <summary onClick={this.loadLangs}>Select Languege</summary>
+                <summary onClick={this.loadLangs}>Select Languege To Search</summary>
                 <div className="selectLang">
-                <select value={this.props.value} onChange={this.props.changeLang}>
-                {this.state.langs.map((element, index) => {
-                    return <option key={index}>{element}</option>
+                <select className="langSelectSearch" value={this.props.value} onChange={this.props.changeLang}>
+
+                {Object.keys(langs).length > 0 && Object.keys(langs).map((key) => {
+                    return <option key={key} value={key} >{langs[key]}</option>
                 })}
+
                 </select>
+
             </div>
             </details>
         )
