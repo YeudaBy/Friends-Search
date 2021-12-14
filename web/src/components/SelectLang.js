6303c6278ae0ff808fef133b3cd7f482964b1d17
yeuda <yeudaborodyanski@gmail.com>
2021-12-14 17:58:57 +0200

    WEB: add label to props

--- web/src/components/SelectLang.js
+++ web/src/components/SelectLang.js
@@ -13,16 +13,15 @@ export default class SelectLang extends React.Component {
         fetch(baseUrl + "language")
             .then((res) => res.json())
             .then((res) => this.setState({langs: res}));
-            console.log(Object.keys(this.state.langs).length)
     }
     
     render() {
         const langs = this.state.langs;
         return (
             <details>
-                <summary onClick={this.loadLangs}>Select Languege To Search</summary>
+                <summary onClick={this.loadLangs}>{this.props.label}</summary>
                 <div className="selectLang">
-                <select className="langSelectSearch" value={this.props.value} onChange={this.props.changeLang}>
+                <select value={this.props.value} onChange={this.props.changeLang}>
 
                 {Object.keys(langs).length > 0 && Object.keys(langs).map((key) => {
                     return <option key={key} value={key} >{langs[key]}</option>
