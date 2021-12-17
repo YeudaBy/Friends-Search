c12804c61766b4f8c42feb9327e0a747aaf4cd1d
yeuda <yeudaborodyanski@gmail.com>
2021-12-17 02:36:52 +0200

    WEB: add HeadNav component to Haed

--- web/src/components/Head.js
+++ web/src/components/Head.js
@@ -1,10 +1,19 @@
 import React from "react";
 import SelectLang from "./SelectLang";
+import HeadNav from "./HeadNav"
 
 export default function Head(props) {
+
+    function changeLange(e) {
+        props.updateLang(
+            e.nativeEvent.target.value
+        )
+    }
+
     return(
-        <>
-            <SelectLang label={"Select site lang"}/>
+        <>  
+            <HeadNav />
+            <SelectLang label={"Select site lang"} changeLang={props.changeLange()}/>
             <h1>Friends Search</h1>
         </>
     )
