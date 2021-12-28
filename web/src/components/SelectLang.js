dc26f0d7aad95d8fe6825022340f41ad41883b4b
yeuda <yeudaborodyanski@gmail.com>
2021-12-28 12:12:25 +0200

    WEB: formatting and comments

--- web/src/components/SelectLang.js
+++ web/src/components/SelectLang.js
@@ -5,31 +5,35 @@ const baseUrl = "http://127.0.0.1:8080/"
 export default class SelectLang extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {langs: {}}
+
+        this.state = { langs: {} }
         this.loadLangs = this.loadLangs.bind(this);
     }
 
-    loadLangs(e){
+    loadLangs(e) {
+        // fetch languages list
         fetch(baseUrl + "language")
             .then((res) => res.json())
-            .then((res) => this.setState({langs: res}));
+            .then((res) => this.setState({ langs: res }));
     }
-    
+
     render() {
         const langs = this.state.langs;
+        let correntLang = (typeof langs[this.props.value] !== "undefined") ? " - " + langs[this.props.value] : " - Default"   // set correntLang
         return (
             <details>
-                <summary onClick={this.loadLangs}>{this.props.label}</summary>
+                <summary onClick={this.loadLangs}>{this.props.label} {correntLang}</summary>  { /* label for language selector */ }
+
                 <div className="selectLang">
-                <select value={this.props.value} onChange={this.props.changeLang}>
+                    <select value={this.props.value} onChange={this.props.updateLang}>
 
-                {Object.keys(langs).length > 0 && Object.keys(langs).map((key) => {
-                    return <option key={key} value={key} >{langs[key]}</option>
-                })}
+                        {Object.keys(langs).length > 0 && Object.keys(langs).map((key) => {
+                            return <option key={key} value={key} >{langs[key]}</option>
+                        })}  { /* all the languages */ }
 
-                </select>
+                    </select>
+                </div>
 
-            </div>
             </details>
         )
     }
