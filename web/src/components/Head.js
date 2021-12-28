da4cd733e4b98e433a989ae8fe4733279fdf6aee
yeuda <yeudaborodyanski@gmail.com>
2021-12-28 12:09:54 +0200

    WEB: pass updateLang arg

--- web/src/components/Head.js
+++ web/src/components/Head.js
@@ -4,7 +4,7 @@ import HeadNav from "./HeadNav"
 export default function Head(props) {
     return (
         <div className="Head">
-            <HeadNav />
+            <HeadNav updateLang={props.updateLang} value={props.lang}/>
             <h1 className="center">Friends Search</h1>
         </div>
     )
