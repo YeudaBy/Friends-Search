738a919a6a939887ba82bd010f56665297206659
yeuda <yeudaborodyanski@gmail.com>
2022-01-02 21:02:02 +0200

    API: add stats and reports

--- Api/main.py
+++ Api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from pony.orm import db_session, RowNotFound
 from Api.Query import Query, Parse
 from flask_cors import CORS, cross_origin
+from Api.stats import create, send_reports
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True  # set pretty print
@@ -14,23 +15,27 @@ app.config['CORS_HEADERS'] = 'Content-Type'  # set cors origin for the site
 @app.route("/")
 def home():
     """ home page """
+    create()
     return render_template("docs.html", content=open("Api/api-references.md").read())
 
 
 @cross_origin()
 @app.route("/language")
 def languages():
+    create()
     return jsonify(Query.all_langs())
 
 
 @cross_origin()
 @app.route("/language/<lang>")
 def get_lang(lang):
+    create()
     return jsonify(Query.is_lang_exist(lang))
 
 
 @app.route("/sentence/random/")
 def random():
+    create()
     res = Query.random()
     return jsonify([Parse(i).__dict__() for i in res])
 
@@ -38,12 +43,14 @@ def random():
 @cross_origin()
 @app.route("/sentence/<int:_id>")
 def get_by_id(_id):
+    create()
     return jsonify(Parse(Query.by_id(_id)).__dict__())
 
 
 @cross_origin()
 @app.route("/sentence/search")
 def search():
+    create()
     error = {}
     response = {}
 
@@ -77,6 +84,14 @@ def search():
         response["results"] = results
 
         return jsonify(response), 200
+
+
+@app.route("/sentence/report", methods=['POST'])
+def report():
+    if request.method == 'POST':
+        _id = request.form.get("id")
+        if send_reports(_id):
+            return {"status": "ok", "id": _id}
 
 
 if __name__ == '__main__':
