c77be65177275da418aee4fff09d2440084116de
yeuda <yeudaborodyanski@gmail.com>
2022-01-02 20:21:21 +0200

    WEB: colorize dotes in head and notranslate

--- web/src/components/Head.js
+++ web/src/components/Head.js
@@ -5,7 +5,9 @@ export default function Head(props) {
     return (
         <div className="Head">
             <HeadNav updateLang={props.updateLang} sLang={props.sLang}/>
-            <h1 className="center">Friends Search</h1>
+            <h1 className="center notranslate">F<span className="red">•</span>r<span className="blue">•</span>
+            i<span className="yelow">•</span>e<span className="red">•</span>n
+            <span className="yelow">•</span>d<span className="blue">•</span>s Search</h1>
         </div>
     )
 }
