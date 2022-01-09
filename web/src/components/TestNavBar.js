e16a582d5522e954ff7473ed7000440a185d6f23
yeuda <yeudaborodyanski@gmail.com>
2022-01-09 20:18:13 +0200

    WEB: fix url

--- web/src/components/TestNavBar.js
+++ web/src/components/TestNavBar.js
@@ -15,7 +15,7 @@ export function NavBar(props) {
             {/* <div className='itemsMenu'> */}
             <a id="home" className="menu-item" href="https://t.me/Friends_SearchBot">{<RiTelegramLine />} Telegram Bot</a>
             <a id="about" className="menu-item" href="https://github.com/YeudaBy/Friends-Search">{<DiGithub />} GitHub</a>
-            <a id="contact" className="menu-item" href="https://api.friends.search.com">{<VscCode />} Api</a>
+            <a id="contact" className="menu-item" href="https://api.friends-search.com">{<VscCode />} Api</a>
             <a className="menu-item" href="https://t.me/RobotTrickSupport">{<GoMention />} Contact </a>
             {/* </div> */}
 
