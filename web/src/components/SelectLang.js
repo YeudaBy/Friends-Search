c91623a0f62dd8dd50d8accb2afbc124fc29c024
yeuda <yeudaborodyanski@gmail.com>
2022-01-11 21:36:32 +0200

    WEB: fix selected value

--- web/src/components/SelectLang.js
+++ web/src/components/SelectLang.js
@@ -3,7 +3,7 @@ import Form from "react-bootstrap/Form"
 export default function SelectLang(props) {
     return (
         <>
-            <Form.Select aria-label="Default select example" onChange={props.updateLang}>
+            <Form.Select onChange={props.updateSearchLang} value={props.value}>
                 <option value="ag">Search with all available languages</option>
                 <option value="en">English</option>
                 <option value="he">עברית</option>
