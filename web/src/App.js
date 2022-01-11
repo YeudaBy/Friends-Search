56761ab2350dd839bc5524a478532839ba7a201b
yeuda <yeudaborodyanski@gmail.com>
2022-01-11 21:35:37 +0200

    WEB: fix langs and vars names

--- web/src/App.js
+++ web/src/App.js
@@ -2,31 +2,28 @@ import { Search } from "./components/Search";
 import Head from "./components/Head";
 import "./App.css"
 import React from "react";
-// import { GoMention } from "react-icons/go"
+import { browserLanguage } from "./languages"
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lang: "en" }
+    this.state = { sLang: browserLanguage() }
 
-    this.updateLang = this.updateLang.bind(this);
+    this.updateSLang = this.updateSLang.bind(this);
   }
 
-  updateLang(event) {
+  updateSLang(event) {
     // console.log(event.target.value)
-    this.setState({ lang: event.target.value },
-      () => {
-        this.state.lang === "ag" && this.setState({ lang: "en" })  // set to english when `all languages` was choosen
-        console.log("set lang to: " + this.state.lang);
-      });
+    this.setState({ sLang: event.target.value },
+      console.log("set site language to: " + this.state.sLang));
   }
 
   render() {
     return (
       <>
-        <div className={this.state.lang}>
-          <Head slang={this.state.lang} updateLang={this.updateLang} />
-          <Search sLang={this.state.lang} />
+        <div className={this.state.sLang}>
+          <Head sLang={this.state.sLang} updateSLang={this.updateSLang} />
+          <Search sLang={this.state.sLang} />
         </div>
       </>
     );
