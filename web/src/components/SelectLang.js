123396cf8665c9bb1a1c213ce5a760f20422220d
yeuda <yeudaborodyanski@gmail.com>
2022-01-12 00:54:49 +0200

    WEB: add label

--- web/src/components/SelectLang.js
+++ web/src/components/SelectLang.js
@@ -1,14 +1,17 @@
 import Form from "react-bootstrap/Form"
+import { MdOutlineLanguage } from "react-icons/md"
 
 export default function SelectLang(props) {
     return (
-        <>
+        <div className="searchLang">
             <Form.Select onChange={props.updateSearchLang} value={props.value}>
-                <option value="ag">Search with all available languages</option>
+                <option value="ag">all available languages</option>
                 <option value="en">English</option>
                 <option value="he">עברית</option>
                 <option value="fr">Français</option>
             </Form.Select>
-        </>
+            <label className="searchLabel">{<MdOutlineLanguage />} Search with language: </label>
+
+        </div>
     )
 }
