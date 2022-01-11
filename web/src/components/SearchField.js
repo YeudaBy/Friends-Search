3e2820cdae200cbf625c7cc0ff2cbc9e499b816e
yeuda <yeudaborodyanski@gmail.com>
2022-01-12 00:55:15 +0200

    WEB: add placeolder

--- web/src/components/SearchField.js
+++ web/src/components/SearchField.js
@@ -4,12 +4,17 @@ import getStr from "../strings";
 export default function SearchField(props) {
     return (
         <div className="searchField center">
-           <form className="searchForm" onSubmit={props.handleSubmit}>
-                <input type="search" name="search" value={props.value} onChange={props.searchChange} className="searchInput"/>
-                {props.value !== "" && <><br /><input 
-                type="submit" 
-                value={getStr("search-btn", props.sLang)} 
-                className="searchBtn"/></>}
+            <form className="searchForm" onSubmit={props.handleSubmit}>
+                <input type="search"
+                    name="search"
+                    value={props.value}
+                    onChange={props.searchChange}
+                    className="searchInput"
+                    placeholder="Unagi..." />
+                {props.value !== "" && <><br /><input
+                    type="submit"
+                    value={getStr("search-btn", props.sLang)}
+                    className="searchBtn" /></>}
             </form>
         </div>
     )
