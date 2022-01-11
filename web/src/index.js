e5ca0a75d7cd29261f8e449f3cddaa73819b4a6f
yeuda <yeudaborodyanski@gmail.com>
2022-01-11 21:32:59 +0200

    WEB: add route to game

--- web/src/index.js
+++ web/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Game } from "./components/Game"
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      {/* <App /> */}
-      <Route path="/" element={<App />} />
+      <Routes>
+        {/* <App /> */}
+        <Route path="/" element={<App />} />
+        <Route path="/game" element={<Game />}/>
       </Routes>
     </BrowserRouter>,
   </React.StrictMode>,
