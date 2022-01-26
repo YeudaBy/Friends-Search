77558e825879107267be3d31958ee69af667f034
yeuda <yeudaborodyanski@gmail.com>
2022-01-26 02:15:17 +0200

    DB: Add verified and favorited, work on test db

--- DB/db.py
+++ DB/db.py
@@ -14,6 +14,8 @@ class Subtitle(db.Entity):
     end = Required(timedelta)       # time of end
     episode = Required(int)
     season = Required(int)
+    verified = Optional(bool)       # if the content is verified
+    favorited = Optional(int)       # count of times that people favorited the sentence
 
     @staticmethod
     def clean_text(text: str) -> str:
@@ -21,31 +23,33 @@ class Subtitle(db.Entity):
         return sub(r"[$&+,:;=?@#|'<>.\-^*()\[\]{}%!~`_\" \n]", "", text).lower()
 
 
-db.bind(provider='sqlite', filename='DataBases/Friends.sqlite', create_db=True)
+# db.bind(provider='sqlite', filename='DataBases/Friends.sqlite', create_db=True)
+db.bind(provider='sqlite', filename='DataBases/Friends.test.sqlite', create_db=True)
+
 db.generate_mapping(create_tables=True)
 # set_sql_debug()
 
 
-@db_session
-def insert_subs(name):
-    # print(name, file)
-    sea, epi = name.split("-")
-    print(sea, epi)
-
-    subs = list(srt.parse(open(name, "r", encoding="ISO-8859-1")))
-
-    for line in subs:
-        if not line.content:
-            continue
-        Subtitle(content=line.content,
-                 raw_content=Subtitle.clean_text(line.content),
-                 start=line.start,
-                 end=line.end,
-                 episode=epi,
-                 season=sea,
-                 lang="fr"
-                 )
-    commit()
+# @db_session
+# def insert_subs(name):
+#     # print(name, file)
+#     sea, epi = name.split("-")
+#     print(sea, epi)
+#
+#     subs = list(srt.parse(open(name, "r", encoding="ISO-8859-1")))
+#
+#     for line in subs:
+#         if not line.content:
+#             continue
+#         Subtitle(content=line.content,
+#                  raw_content=Subtitle.clean_text(line.content),
+#                  start=line.start,
+#                  end=line.end,
+#                  episode=epi,
+#                  season=sea,
+#                  lang="fr"
+#                  )
+#     commit()
 
 
 # ========== Insert Data to DB =======
@@ -55,4 +59,8 @@ def insert_subs(name):
 # insert_subs("10-17")
 # print("10-17", "insert success!")
 
+# with db_session:
+#     for i in Subtitle.select():
+#         i.favorited = 0
+#     commit()
 
