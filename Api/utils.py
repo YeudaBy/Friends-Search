cca063e2c0dcded779382c0bf2ccc39d72fa9f31
yeuda <yeudaborodyanski@gmail.com>
2022-01-31 02:52:31 +0200

    API: app config class

---
+++ Api/utils.py
@@ -0,0 +1,6 @@
+class AppConfig:
+    """ the custom config for the app """
+    JSONIFY_PRETTYPRINT_REGULAR = True  # set pretty print
+    JSON_AS_ASCII = False  # set unicode support for hebrew
+    JSON_SORT_KEYS = False  # return with our order
+    CORS_HEADERS = "Content - Type"
